70876131c313e7329ade188ff5566a2fc13fd01e
ZRY551 <zryyou163@163.com>
2023-09-25 21:39:54 +0800

    init

---
+++ udp-im/smsg-easy.py
@@ -0,0 +1,5 @@
+from socket import *;s=socket(AF_INET,SOCK_DGRAM);s.bind(("0.0.0.0",65000));s.setsockopt(SOL_SOCKET,SO_BROADCAST,1);n=input("Name>")
+while True:
+    m = input("Input>")
+    if not m: continue
+    m=n+":"+m;s.sendto(m.encode("utf-8"),("255.255.255.255",65000))
