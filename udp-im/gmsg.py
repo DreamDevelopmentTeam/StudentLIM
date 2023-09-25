b0d497a0bb8cea24b1d010332e49b6386848b4d7
ZRY551 <zryyou163@163.com>
2023-09-25 21:33:39 +0800

    init

---
+++ udp-im/gmsg.py
@@ -0,0 +1,7 @@
+from socket import *
+s = socket(AF_INET, SOCK_DGRAM)
+s.bind(("0.0.0.0", 65000))
+s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+while True:
+    d, a = s.recvfrom(1024)
+    print(a, " => ", d.decode("utf-8"))
